f452110dc33721da59a8f1949c16c333bde2e695
staskosh <staskoshenkov@rambler.ru>
2022-04-20 13:56:07 +0300

    Setup DialogFlow.

--- publiching_assistense_bot.py
+++ publiching_assistense_bot.py
@@ -4,6 +4,8 @@ from environs import Env
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
+from google.cloud import dialogflow
+
 env = Env()
 env.read_env()
 
@@ -23,35 +25,42 @@ def start(update: Update, context: CallbackContext) -> None:
     )
 
 
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=texts, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+
+    return response.query_result.fulfillment_text
+
+
 def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
 def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+    project_id = env('GC_PROJECT_ID')
+    user = update.effective_user
+    session_id = user.id
+    language_code = env('LANGUAGE_CODE')
+    reply = detect_intent_texts(project_id, session_id, update.message.text, language_code)
+    update.message.reply_text(reply)
 
 
 def main() -> None:
     tg_token = env("TG_TOKEN")
-
+    GOOGLE_APPLICATION_CREDENTIALS = env('GOOGLE_APPLICATION_CREDENTIALS')
     updater = Updater(tg_token)
-
-    # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
-
-    # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
 
-    # Start the Bot
     updater.start_polling()
 
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
     updater.idle()
 
 
